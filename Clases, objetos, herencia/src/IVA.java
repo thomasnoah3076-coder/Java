a1694fc6d9670aacdceb1bfdbebc7a2ff67e3812
Thomas Chaparro <thomasnoah3076@gmail.com>
2026-04-27 22:16:42 -0500

    Comentarios explicativos, actualizaciones con polimorfismo, abstraccion.

--- Clases, objetos, herencia/src/IVA.java
+++ Clases, objetos, herencia/src/IVA.java
@@ -2,6 +2,6 @@
 // o con retorno, con paramertro o sin parametros, pero sin cuerpo o bloque de
 // codigo, ya que el encargado de definirlos es la clase con la que se implementan
 // o en otras palabras con las que firman un contrato.
-public interface IVA {
+interface IVA {
     public double calcularIVA(double basePrice);
 }
